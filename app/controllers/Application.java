fbb28ec816485fc1513bc0dca7f71e0ec23929a0
Scarlat Mihai <scarlatmihai25@gmail.com>
2019-12-22 21:49:51 +0200

    add example of api route

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.Date;
 
 public class Application extends Controller {
 
@@ -11,8 +15,17 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
-    public Result hello(){
+    public Result chat()
+    {
         return ok(hello.render("Hello world"));
     }
 
+    public Result api()
+    {
+        ObjectNode result = Json.newObject();
+        result.put("messageId", 1);
+        result.put("userName", "Mihai");
+        result.put("time", new Date().toString());
+        return ok(result);
+    }
 }
